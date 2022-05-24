7a867b1f7d76eb85d5f8580b418544c835bd1d8d
vnistala14 <101360356+vnistala14@users.noreply.github.com>
2022-05-23 20:20:31 -0700

    Update OrderServiceApplication.java
    removed sorting

--- src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
@@ -21,8 +21,7 @@ public class OrderServiceApplication {
 
     @GetMapping
     public List<Order> fetchOrders() {
-        return orderDao.getOrders().stream().
-                sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList());
+        return orderDao.getOrders();
     }
 
     public static void main(String[] args) {
